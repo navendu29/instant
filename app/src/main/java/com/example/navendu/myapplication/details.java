7247d425e4f3853e3cdc8ec8a4f35447fd04ed03
navendu29 <navendugupta29@gmail.com>
2017-05-21 04:37:32 +0530

    instant

---
+++ app/src/main/java/com/example/navendu/myapplication/details.java
@@ -0,0 +1,19 @@
+package com.example.navendu.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+}
